b3d06d4a83122e9e59eb1a54ded49c211a49318f
Sahil Kumar <sahilkumar@Sahils-MacBook-Air.local>
2023-11-23 14:20:43 +1100

    add basic styling consistent with Figma mockup

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -4,7 +4,7 @@ import styles from './styles/WelcomeBanner.module.css';
 function WelcomeBanner( {firstName} ) {
   return (
     <div className={styles.welcomeBanner}>
-      <h1>GradApp</h1>
+      <h1 className={styles.appTitle}>GradApp</h1>
       <h1>Welcome, {firstName}</h1>
     </div>
   );
